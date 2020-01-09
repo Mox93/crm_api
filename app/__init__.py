1c148753614acb63a7d7b2dd70fb8b517a7f966e
Mohamed Ragaiy Saleh <mohamed.ragaiy.saleh@gmail.com>
2020-01-09 23:04:46 +0300

    Managing imports and dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
+# App
 from flask import Flask
 
-# App
 app = Flask(__name__)
 app.config["HOST"] = "localhost"
 app.config["PORT"] = 5000
 app.config["DEBUG"] = True
+
+
+# Cross-Origin Resource Sharing
+from flask_cors import CORS
+
+CORS(app)
 
 
 # JWT
@@ -27,8 +33,16 @@ db.init_app(app)
 app.session_interface = MongoEngineSessionInterface(db)
 
 
+# GraphQL API
 from flask_graphql import GraphQLView
 from api import schema
+from api.auth import add_claims_to_access_token, user_identity_lookup
 
 app.add_url_rule('/api', view_func=GraphQLView.as_view('api', schema=schema, graphiql=True))
+
+
+# Url
+from web import web
+
+app.register_blueprint(web)
 
